2cf4ae4085826037bc2eb84d3e9c0dd6dcc9bbed
Bram Kuijper <bramkuijper@gmail.com>
2026-07-13 11:28:35 +0100

    various small changes, sort-of in runnable form now

--- src/ibm/main.cpp
+++ src/ibm/main.cpp
@@ -6,11 +6,43 @@ int main(int argc, char **argv)
 {
     Parameters params{}; // start parameters with all the default settings
 
-    params.file_name = argv[1]; // then override defaults with command-line args
-    params.n_sites = static_cast<unsigned>(
-            std::stoul(argv[2])); // then override defaults with command-line args
+    // then override defaults with command-line args
+    params.file_name = argv[1]; 
 
+    params.n_sites = static_cast<unsigned>(
+            std::stoul(argv[2])); 
+    
     assert(params.n_sites > 1);
+
+    params.max_generation = static_cast<unsigned>(
+            std::stoul(argv[3]));
+    
+    params.max_season_time_steps = static_cast<unsigned>(
+            std::stoul(argv[4]));
+
+    params.mu_an = std::stod(argv[5]);
+    params.mu_bn = std::stod(argv[6]);
+
+    params.mu_ax = std::stod(argv[7]);
+    params.mu_bx = std::stod(argv[8]);
+    
+    params.mu_at = std::stod(argv[9]);
+    params.mu_bt = std::stod(argv[10]);
+    
+    params.mu_ap = std::stod(argv[11]);
+    params.mu_bp = std::stod(argv[12]);
+    
+    params.mu_anu = std::stod(argv[13]);
+    params.mu_bnu = std::stod(argv[14]);
+    
+    params.mu_axo = std::stod(argv[15]);
+    params.mu_bxo = std::stod(argv[16]);
+
+    params.pr_base_flight_survive = std::stod(argv[17]);
+    params.flight_survive_scale = std::stod(argv[18]);
+    params.flight_survive_power = std::stod(argv[19]);
+    params.f = std::stod(argv[20]);
+    params.g = std::stod(argv[21]);
 
     Simulation sim{params};
 
--- src/ibm/vary_parameters.py
+++ src/ibm/vary_parameters.py
@@ -7,3 +7,84 @@ base_name = "sim_cue_migration_"
 current_time = dt.datetime.now()
 base_name += current_time.strftime("%Y%m%d_%H%M%S")
 
+n_sites = 10
+
+max_generation = 100
+max_season_time_steps = 50
+
+mu_n = [0.02]
+mu_x = [0.02]
+mu_t = [0.02]
+mu_nu = [0.02]
+mu_xo = [0.02]
+
+mu_ap = 0
+mu_bp = 0
+
+
+
+nrep = 3
+ctr = 1
+
+pr_base_flight_survive = 0.1
+flight_survive_scale = 0.7
+flight_survive_power = 1.0 
+
+f = [1.0]
+g = [3.0]
+
+
+exe = "./migration_cues.exe"
+
+for i in range(0,nrep):
+    for mu_n_i in mu_n:
+
+        mu_an = mu_n_i
+        mu_bn = mu_n_i
+
+        for mu_x_i in mu_x:
+            mu_ax = mu_x_i
+            mu_bx = mu_x_i
+
+            for mu_t_i in mu_t:
+                mu_at = mu_t_i
+                mu_bt = mu_t_i
+
+                for mu_nu_i in mu_nu:
+                    mu_anu = mu_nu_i
+                    mu_bnu = mu_nu_i
+
+                    for mu_xo_i in mu_xo:
+                        mu_axo = mu_xo_i
+                        mu_bxo = mu_xo_i
+
+                        for f_i in f:
+                            for g_i in g:
+
+                                file_name = f"{base_name}_{ctr}"
+
+                                ctr +=1
+
+                                job_str = f"{exe} " +\
+                                            f"{file_name} " +\
+                                            f"{max_generation} " +\
+                                            f"{max_season_time_steps} " +\
+                                            f"{mu_an} " +\
+                                            f"{mu_bn} " +\
+                                            f"{mu_ax} " +\
+                                            f"{mu_bx} " +\
+                                            f"{mu_at} " +\
+                                            f"{mu_bt} " +\
+                                            f"{mu_ap} " +\
+                                            f"{mu_bp} " +\
+                                            f"{mu_anu} " +\
+                                            f"{mu_bnu} " +\
+                                            f"{mu_axo} " +\
+                                            f"{mu_bxo} " +\
+                                            f"{pr_base_flight_survive} " +\
+                                            f"{flight_survive_scale} " +\
+                                            f"{flight_survive_power} " +\
+                                            f"{f_i} " +\
+                                            f"{g_i} "
+
+                                print(job_str)
